5040455d3fa28dcb1c64a99332f5fef537dfacac
Miniieee <bagyi.tomi93@gmail.com>
2025-03-22 11:13:40 +0000

    added id

--- lib/main.dart
+++ lib/main.dart
@@ -7,71 +7,121 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Root of the application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Todo List', home: TodoListPage());
+    return MaterialApp(title: 'Shopping List', home: ShoppingListPage());
   }
 }
 
-// Model for a todo item.
-class Todo {
+// Model for a shopping item.
+class ShoppingItem {
   final String title;
-  bool isDone;
+  bool isBought;
 
-  Todo({required this.title, this.isDone = false});
+  ShoppingItem({required this.title, this.isBought = false, this.id = 0});
+  int id;
 }
 
-class TodoListPage extends StatefulWidget {
+class ShoppingListPage extends StatefulWidget {
   @override
-  _TodoListPageState createState() => _TodoListPageState();
+  _ShoppingListPageState createState() => _ShoppingListPageState();
 }
 
-class _TodoListPageState extends State<TodoListPage> {
-  List<Todo> todos = [
-    Todo(title: 'Buy groceries'),
-    Todo(title: 'Clean the house'),
-    Todo(title: 'Finish homework'),
-    Todo(title: 'Call mom'),
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  // Category lists.
+  List<ShoppingItem> bakeryItems = [
+    ShoppingItem(title: 'Bread', isBought: false, id: 1),
+    ShoppingItem(title: 'Bagels', isBought: false, id: 2),
+    ShoppingItem(title: 'Muffins', isBought: false, id: 3),
+    ShoppingItem(title: 'Croissants', isBought: false, id: 4),
+    ShoppingItem(title: 'Baguette', isBought: false, id: 5),
   ];
 
-  void _toggleTodo(int index) {
+  List<ShoppingItem> dairyItems = [
+    ShoppingItem(title: 'Milk', isBought: false, id: 6),
+    ShoppingItem(title: 'Cheese', isBought: false, id: 7),
+    ShoppingItem(title: 'Yogurt', isBought: false, id: 8),
+    ShoppingItem(title: 'Butter', isBought: false, id: 9),
+    ShoppingItem(title: 'Cream', isBought: false, id: 10),
+  ];
+
+  List<ShoppingItem> snacksItems = [
+    ShoppingItem(title: 'Chips', isBought: false, id: 11),
+    ShoppingItem(title: 'Cookies', isBought: false, id: 12),
+    ShoppingItem(title: 'Candy', isBought: false, id: 13),
+    ShoppingItem(title: 'Nuts', isBought: false, id: 14),
+    ShoppingItem(title: 'Granola Bars', isBought: false, id: 15),
+  ];
+
+  // Toggle the item's status and reposition it within its category.
+  void _toggleItem(List<ShoppingItem> items, int index) {
     setState(() {
-      if (!todos[index].isDone) {
-        // Mark as done, add strike-through, and move to bottom.
-        todos[index].isDone = true;
-        final todo = todos.removeAt(index);
-        todos.add(todo);
+      if (!items[index].isBought) {
+        // Mark as bought, add strike-through, and move to bottom.
+        items[index].isBought = true;
+        final item = items.removeAt(index);
+        items.add(item);
       } else {
-        // Remove strike-through and move back to the top.
-        todos[index].isDone = false;
-        final todo = todos.removeAt(index);
-        todos.insert(0, todo);
+        // Mark as not bought, remove strike-through, and move to top.
+        items[index].isBought = false;
+        final item = items.removeAt(index);
+        items.insert(0, item);
       }
     });
+  }
+
+  // Build a section for a given category.
+  Widget _buildCategorySection(String categoryName, List<ShoppingItem> items) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Category header.
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            categoryName,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        // List of items in this category.
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return ListTile(
+              title: Text(
+                item.title,
+                style: TextStyle(
+                  decoration:
+                      item.isBought
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                ),
+              ),
+              onTap: () => _toggleItem(items, index),
+            );
+          },
+        ),
+        const Divider(), // Optional divider between sections.
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Todo List')),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final todo = todos[index];
-          return ListTile(
-            title: Text(
-              todo.title,
-              style: TextStyle(
-                decoration:
-                    todo.isDone
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none,
-              ),
-            ),
-            onTap: () => _toggleTodo(index),
-          );
-        },
+      appBar: AppBar(title: const Text('Shopping List')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildCategorySection('Bakery', bakeryItems),
+            _buildCategorySection('Dairy', dairyItems),
+            _buildCategorySection('Snacks', snacksItems),
+          ],
+        ),
       ),
     );
   }
